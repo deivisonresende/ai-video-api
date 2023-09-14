e80e3e918921620c3f1d5ec2e9ef0bc4b2150f93
Deivison Resende <deivison4844@gmail.com>
2023-09-14 00:41:02 -0300

    feat:Setup cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import { fastify } from "fastify";
 import { v1Routes } from "./api/v1/routes";
+import fastifyCors from "@fastify/cors";
 
 const app = fastify()
 
 app
+  .register(fastifyCors, { origin: '*' })
   .register(v1Routes, { prefix: '/v1' })
   .listen({ port: 3333 })
   .then(() => console.log('HTTP server listening on port 3333'))
+  
